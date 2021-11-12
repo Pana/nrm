c0328a93eadff2a00c0d138d9838adb6a16c8cd3
CaptainOfPhB <jiangtao.dai@foxmail.com>
2021-11-12 21:27:33 +0800

    add helper.js

--- helpers.js
+++ helpers.js
@@ -1,7 +1,31 @@
+const fs = require('fs');
+const ini = require('ini');
 const chalk = require('chalk');
 
+const { NRMRC, NPMRC, REGISTRIES } = require('./constants');
+
+function readFile(file) {
+  return fs.existsSync(file) ? ini.parse(fs.readFileSync(file, 'utf-8')) : {};
+}
+
+async function writeFile(path, content) {
+  return new Promise((resolve, reject) => {
+    try {
+      fs.writeFileSync(path, ini.stringify(content));
+      resolve();
+    } catch (error) {
+      reject(error);
+      exit(error);
+    }
+  });
+}
+
 function padding(message = '', before = 1, after = 1) {
-  return new Array(before).join(' ') + message + new Array(after).join(' ');
+  return new Array(before).fill(' ').join('') + message + new Array(after).fill(' ').join('');
+}
+
+function printSuccess(message) {
+  console.error(chalk.bgGreenBright(padding('SUCCESS')) + ' ' + message);
 }
 
 function printError(error) {
@@ -19,6 +43,44 @@ function geneDashLine(message, length) {
   return padding(chalk.dim(finalMessage));
 }
 
+function isLowerCaseEqual(str1, str2) {
+  if (str1 && str2) {
+    return str1.toLowerCase() === str2.toLowerCase();
+  } else {
+    return !str1 && !str2;
+  }
+}
+
+async function getCurrentRegistry() {
+  return new Promise((resolve, reject) => {
+    let npmrc;
+    try {
+      npmrc = readFile(NPMRC);
+    } catch (error) {
+      reject(error);
+      printError(error);
+    }
+    if (npmrc) {
+      resolve(npmrc.registry);
+    }
+  });
+}
+
+async function getRegistries() {
+  return new Promise((resolve, reject) => {
+    let customRegistries;
+    try {
+      customRegistries = readFile(NRMRC);
+    } catch (error) {
+      reject(error);
+      exit(error);
+    }
+    if (customRegistries) {
+      resolve(Object.assign({}, REGISTRIES, customRegistries));
+    }
+  });
+}
+
 function exit(error) {
   printError(error);
   process.exit(1);
@@ -27,6 +89,11 @@ function exit(error) {
 module.exports = {
   exit,
   geneDashLine,
-  printError,
+  printSuccess,
   printMessages,
+  isLowerCaseEqual,
+  readFile,
+  writeFile,
+  getRegistries,
+  getCurrentRegistry,
 };
