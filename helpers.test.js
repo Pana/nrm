4139f8d2fd564d03aca6cdb35e940e0d493fb877
CaptainOfPhB <jiangtao.dai@foxmail.com>
2021-11-14 15:24:33 +0800

    add helper.test.js

---
+++ helpers.test.js
@@ -0,0 +1,127 @@
+const ini = require('ini');
+const tap = require('tap');
+const chalk = require('chalk');
+const { beforeEach, test, mock } = tap;
+const { stdout, stderr } = require('test-console');
+
+const { NPMRC, NRMRC, REGISTRY } = require('./constants');
+
+// ========== mock `fs` within helpers.js ==========
+
+let mockedFiles = Object.create(null);
+
+function readFileSync(path) {
+  return mockedFiles[path];
+}
+
+function writeFileSync(path, content) {
+  mockedFiles[path] = content;
+}
+
+function existsSync(path) {
+  return path in mockedFiles;
+}
+
+const helpers = mock('./helpers.js', {
+  fs: {
+    existsSync,
+    readFileSync,
+    writeFileSync,
+  },
+});
+
+// ========== test cases ==========
+
+beforeEach(() => {
+  mockedFiles = Object.create(null);
+  mockedFiles[NPMRC] = '';
+  mockedFiles[NRMRC] = '';
+});
+
+test('geneDashLine', t => {
+  const result1 = helpers.geneDashLine('taobao', 10);
+  const result2 = helpers.geneDashLine('taobao', 1);
+  t.equal(result1, ` ${chalk.dim('-----')} `, 'geneDashLine(taobao, 10) equal to ` ----- `');
+  t.equal(result2, ` ${chalk.dim('-')} `, 'geneDashLine(taobao, 1) equal to ` - `');
+  t.end();
+});
+
+test('getCurrentRegistry', t => {
+  const registry = ' https://registry.npmjs.org/';
+  writeFileSync(NPMRC, ini.stringify({ [REGISTRY]: registry }));
+  helpers
+    .getCurrentRegistry()
+    .then(currentRegistry => {
+      t.equal(currentRegistry, registry, 'can get the current registry in use');
+      t.end();
+    });
+});
+
+test('getRegistries', t => {
+  const name = 'fake name';
+  const registry = 'https://registry.example.com/';
+  writeFileSync(NRMRC, ini.stringify({ [name]: { registry } }));
+  helpers
+    .getRegistries()
+    .then(registries => {
+      t.equal(Object.keys(registries).includes(name), true, 'can get all registries');
+      t.end();
+    });
+});
+
+test('readFile', async t => {
+  const content = 'hello nrm';
+  writeFileSync(NRMRC, ini.stringify({ content: content }));
+  const result1 = helpers.readFile(NRMRC);
+  const result2 = helpers.readFile('file not exist');
+  t.equal(result1.content, content, 'can get the file content');
+  t.same(result2, Object.create(null), 'can get an empty object when file is not exist');
+  t.end();
+});
+
+test('writeFile', t => {
+  const content = { nrm: 'nrm is great' };
+  helpers
+    .writeFile(NRMRC, { content })
+    .then(() => {
+      const result = helpers.readFile(NRMRC);
+      t.same(result.content, content, 'can write content to the file');
+      t.end();
+    });
+});
+
+test('isLowerCaseEqual', t => {
+  const result1 = helpers.isLowerCaseEqual('taobao', 'TAOBAO');
+  const result2 = helpers.isLowerCaseEqual('jd', 'tb');
+  const result3 = helpers.isLowerCaseEqual('taobao', '');
+  const result4 = helpers.isLowerCaseEqual('', '');
+  t.equal(result1, true, '`taobao` is considered equal to `TAOBAO`');
+  t.equal(result2, false, '`jd` is not equal to `tb`');
+  t.equal(result3, false, '`taobao` is not equal to an empty string');
+  t.equal(result4, true, 'two empty strings are considered equal');
+  t.end();
+});
+
+test('printError', t => {
+  const error = 'an error';
+  const output = stderr.inspectSync(() => helpers.printError(error));
+  t.same(output, printLineByLine(chalk.bgRed(' ERROR ') + ' ' + chalk.red(error)), 'can print an error message');
+  t.end();
+});
+
+test('printSuccess', t => {
+  const message = 'hello nrm';
+  const output = stdout.inspectSync(() => helpers.printSuccess(message));
+  t.same(output, printLineByLine(chalk.bgGreenBright(' SUCCESS ') + ' ' + message), 'can print a success message');
+  t.end();
+});
+
+test('printMessages', t => {
+  const output = stdout.inspectSync(() => helpers.printMessages(['hello', 'nrm']));
+  t.same(output, printLineByLine('hello', 'nrm'), 'can print a group messages');
+  t.end();
+});
+
+function printLineByLine(...lines) {
+  return lines.map(line => line + '\n');
+}
