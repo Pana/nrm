c49a0a46a3fd2e7d794a78f386005196f4e6a104
HomyeeKing <HomyeeKing@gmail.com>
2025-07-11 10:57:23 +0800

    feat: omit home field to respect the npmrc

--- src/index.ts
+++ src/index.ts
@@ -16,90 +16,98 @@ import {
   onSetScope,
   onTest,
   onUse,
+  cleanInvalidNpmConfig,
 } from './actions.js';
 
-const program = new Command();
+async function main() {
+  const program = new Command();
 
-const { name, version, description } = packageJson;
-program.name(name).description(description).version(version);
+  // make sure the invalid npm config is cleaned up
+  await cleanInvalidNpmConfig();
 
-program.command('ls').description('List all the registries').action(onList);
+  const { name, version, description } = packageJson;
+  program.name(name).description(description).version(version);
 
-program
-  .command('current')
-  .option('-u, --show-url', 'Show the registry URL instead of the name')
-  .description('Show current registry name or URL')
-  .action(onCurrent);
+  program.command('ls').description('List all the registries').action(onList);
 
-program
-  .command('use [name]')
-  .description('Change current registry')
-  .action(onUse);
+  program
+    .command('current')
+    .option('-u, --show-url', 'Show the registry URL instead of the name')
+    .description('Show current registry name or URL')
+    .action(onCurrent);
 
-program
-  .command('add <name> <url> [home]')
-  .description('Add custom registry')
-  .action(onAdd);
+  program
+    .command('use [name]')
+    .description('Change current registry')
+    .action(onUse);
 
-program
-  .command('login <name> [base64]')
-  .option('-a, --always-auth', 'Set is always auth')
-  .option('-u, --username <username>', 'Your user name for this registry')
-  .option('-p, --password <password>', 'Your password for this registry')
-  .option('-e, --email <email>', 'Your email for this registry')
-  .description(
-    'Set authorize information for a custom registry with a base64 encoded string or username and password',
-  )
-  .action(onLogin);
+  program
+    .command('add <name> <url> [home]')
+    .description('Add custom registry')
+    .action(onAdd);
 
-program
-  .command('set-hosted-repo <name> <repo>')
-  .description(
-    'Set hosted npm repository for a custom registry to publish package',
-  )
-  .action(onSetRepository);
+  program
+    .command('login <name> [base64]')
+    .option('-a, --always-auth', 'Set is always auth')
+    .option('-u, --username <username>', 'Your user name for this registry')
+    .option('-p, --password <password>', 'Your password for this registry')
+    .option('-e, --email <email>', 'Your email for this registry')
+    .description(
+      'Set authorize information for a custom registry with a base64 encoded string or username and password',
+    )
+    .action(onLogin);
 
-program
-  .command('set-scope <scopeName> <url>')
-  .description('Associating a scope with a registry')
-  .action(onSetScope);
+  program
+    .command('set-hosted-repo <name> <repo>')
+    .description(
+      'Set hosted npm repository for a custom registry to publish package',
+    )
+    .action(onSetRepository);
 
-program
-  .command('del-scope <scopeName>')
-  .description('Remove a scope')
-  .action(onDeleteScope);
+  program
+    .command('set-scope <scopeName> <url>')
+    .description('Associating a scope with a registry')
+    .action(onSetScope);
 
-program
-  .command('set <name>')
-  .requiredOption('-a,--attr <attr>', 'Set a custom registry attribute')
-  .requiredOption('-v,--value <value>', 'Set a custom registry value')
-  .description('Set a custom registry attribute')
-  .action(onSetAttribute);
+  program
+    .command('del-scope <scopeName>')
+    .description('Remove a scope')
+    .action(onDeleteScope);
 
-program
-  .command('rename <name> <newName>')
-  .description('Change custom registry name')
-  .action(onRename);
+  program
+    .command('set <name>')
+    .requiredOption('-a,--attr <attr>', 'Set a custom registry attribute')
+    .requiredOption('-v,--value <value>', 'Set a custom registry value')
+    .description('Set a custom registry attribute')
+    .action(onSetAttribute);
 
-program
-  .command('del [name]')
-  .description('Delete custom registry')
-  .action(onDelete);
+  program
+    .command('rename <name> <newName>')
+    .description('Change custom registry name')
+    .action(onRename);
 
-program
-  .command('home <name> [browser]')
-  .description('Open the homepage of registry with optional browser')
-  .action(onHome);
+  program
+    .command('del [name]')
+    .description('Delete custom registry')
+    .action(onDelete);
 
-program
-  .command('test [registry]')
-  .description('Show response time for specific or all registries')
-  .action(() => {
-    onTest();
-  }); // ignore return value to pass typescript check
+  program
+    .command('home <name> [browser]')
+    .description('Open the homepage of registry with optional browser')
+    .action(onHome);
 
-program.parse(process.argv);
+  program
+    .command('test [registry]')
+    .description('Show response time for specific or all registries')
+    .action(() => {
+      onTest();
+    }); // ignore return value to pass typescript check
 
-if (process.argv.length === 2) {
-  program.outputHelp();
+  await program.parseAsync(process.argv);
+
+  if (process.argv.length === 2) {
+    program.outputHelp();
+  }
 }
+
+main();
